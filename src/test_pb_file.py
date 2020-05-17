e00d4efa2f994c83314a304d45f9050f310c7c63
BogdanM1 <dd11eeaann@gmail.com>
2020-05-17 13:20:51 +0200

    tensorflow - c

--- src/test_pb_file.py
+++ src/test_pb_file.py
@@ -2,16 +2,15 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
-sample = np.array([.1, .1, .1, .1, .1, .1])
+sample = np.array( [[.3, .4, .4, .13, .3, .1]] )
 
 with tf.Session() as sess:	    
 	with gfile.FastGFile('../models/model.pb', 'rb') as f:	        
 		graph_def = tf.GraphDef()	        
 		graph_def.ParseFromString(f.read())	        
 		sess.graph.as_default()	        
-		g_in = tf.import_graph_def(graph_def)
-
-tensor_output = sess.graph.get_tensor_by_name('dense_1/kernel')	    
-tensor_input = sess.graph.get_tensor_by_name('dense_4/kernel')	    
-predictions = sess.run(tensor_output, {tensor_input:sample})	    
-print(predictions)
+		g_in = tf.import_graph_def(graph_def)	        
+		tensor_input = sess.graph.get_tensor_by_name('import/dense_1_input:0')	        
+		tensor_output = sess.graph.get_tensor_by_name('import/dense_4/kernel:0')	        
+		predictions = sess.run(tensor_output, {tensor_input:sample})	        
+		print(predictions)

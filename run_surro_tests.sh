ecced336a04f5c4ba7baa7e74554ba6f10df1ec3
BogdanM1 <dd11eeaann@gmail.com>
2020-07-20 22:29:35 +0200

    run surro tests

--- run_surro_tests.sh
+++ run_surro_tests.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
 cd surro-muscle/src/
 python3 convert_h5_to_pb.py
 cd $HOME/boxieMexie
 ./run_tests.sh
-while((ps -u bogdan|grep FEM_MPI|wc -l) > 0); do sleep 1; done
+
+num_fem_mpi=`ps -u bogdan|grep FEM_MPI|wc -l`
+while [ $num_fem_mpi != 0 ]
+do
+    sleep 1 
+    num_fem_mpi=`ps -u bogdan|grep FEM_MPI|wc -l`
+done
 ./collect_results.sh >../surro-muscle/results/metrics.txt
